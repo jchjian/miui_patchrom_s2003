fb34e47e7707420453d73f677ef04fd11adc0db5
jchjian <671346@qq.com>
2015-01-18 16:38:02 +0800

    add file_contexts into target_files

---
+++ customize_target_files.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+echo ">>> add file_contexts into target_files "
+cp -f other/file_contexts out/target_files/META/
---
+++ releasetools.py
@@ -0,0 +1,16 @@
+import common
+import edify_generator
+
+def WritePolicyConfig(info):
+  try:
+    file_contexts = info.input_zip.read("META/file_contexts")
+    common.ZipWriteStr(info.output_zip, "file_contexts", file_contexts)
+  except KeyError:
+    print "warning: file_context missing from target;"
+
+
+def FullOTA_InstallEnd(info):
+    WritePolicyConfig(info)
+
+
+
