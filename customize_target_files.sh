d2e19163d10bcb3c97be151a8b06265b130c22a4
jchjian <671346@qq.com>
2015-01-18 16:47:01 +0800

    use installd of vendor

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -5,3 +5,6 @@ cp -f other/file_contexts out/target_files/META/
 
 echo ">>> remove some not need files of vendor "
 rm -rf out/target_files/SYSTEM/etc/custom_config
+
+echo ">>> use installd of vendor "
+cp -rf stockrom/system/bin/installd out/target_files/SYSTEM/bin
