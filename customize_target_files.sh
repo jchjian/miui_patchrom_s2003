414ee7e2224efcfdd52008a7dfb303cf4ee7386a
jchjian <671346@qq.com>
2015-01-18 16:43:26 +0800

    remove some not need files of vendor

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -2,3 +2,6 @@
 
 echo ">>> add file_contexts into target_files "
 cp -f other/file_contexts out/target_files/META/
+
+echo ">>> remove some not need files of vendor "
+rm -rf out/target_files/SYSTEM/etc/custom_config
