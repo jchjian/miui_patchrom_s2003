c856598a9425c5975a217007a1574f768617c395
jchjian <671346@qq.com>
2015-01-18 16:50:01 +0800

    reset lbesec permissions

--- releasetools.py
+++ releasetools.py
@@ -1,6 +1,14 @@
 import common
 import edify_generator
 
+
+def AddArgsForSetPermission(info):
+  edify = info.script
+  for i in xrange(len(edify.script)):
+    if "set_perm(" in edify.script[i] and "/system/xbin/lbesec" in edify.script[i]:
+      edify.script[i] = 'set_perm(0, 0, 06755, "/system/xbin/lbesec");'
+      return
+
 def WritePolicyConfig(info):
   try:
     file_contexts = info.input_zip.read("META/file_contexts")
@@ -10,6 +18,7 @@ def WritePolicyConfig(info):
 
 
 def FullOTA_InstallEnd(info):
+    AddArgsForSetPermission(info)
     WritePolicyConfig(info)
 
 
